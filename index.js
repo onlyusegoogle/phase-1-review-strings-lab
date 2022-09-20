998136fc5acc92ec1221dd6da51d507243ef9521
mason white <masondwhite1@gmail.com>
2022-09-20 11:04:31 -0600

    commit tree 093c320cf6b4611221a73609f3fdde55f4ccbcfe

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Mason White"
+const firstLetter = currentUser.charAt(0)
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const shortGreeting = `Welcome, ${firstLetter}!`
